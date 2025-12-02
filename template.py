962e89be1d065885e369f54a08fde5b3a471c3e0
Istvan Kleijn <istvan.kleijn@icr.ac.uk>
2025-12-02 15:46:40 +0000

    template and gitignore from '24

---
+++ template.py
@@ -0,0 +1,50 @@
+import pathlib 
+
+
+DAY = 0
+
+
+def read_file(day: int):
+    file = pathlib.Path("data") / f"{day}.txt"
+    with open(file, "r") as handle:
+        lines = handle.readlines()
+    return(lines)
+
+def parse(lines, *, debug=False):
+    pass
+
+class Object:
+    def __init__(self):
+        pass
+    
+    def run_analysis(self, *, debug=False):
+        pass
+    
+    def get_answer1(self, *, debug=False):
+        return None
+
+    def get_answer2(self, *, debug=False):
+        return None
+    
+    def get_answers(self, *, debug=False):
+        answer1 = self.get_answer1(debug=debug)
+        answer2 = self.get_answer2(debug=debug)
+        return answer1, answer2
+
+test_input = """
+
+""".strip()
+def test():
+    lines = test_input.splitlines()
+    object = parse(lines, debug=True)
+    object.run_analysis(debug=True)
+    test1, test2 = object.get_answers(debug=True)
+    print(f"Test 1: {test1}\nTest 2: {test2}")
+
+if __name__ == "__main__":
+    test()
+    lines = read_file(DAY)
+    object = parse(lines)
+    object.run_analysis()
+    answer1, answer2 = object.get_answers()
+    print(f"Answer 1: {answer1}\nAnswer 2: {answer2}")
