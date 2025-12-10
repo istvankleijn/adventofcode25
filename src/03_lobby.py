8632fb0b46d994bbad88b5cfff54d3260ebc38ad
Istvan Kleijn <istvan.kleijn@icr.ac.uk>
2025-12-10 18:31:53 +0000

    3-2: Lobby

--- src/03_lobby.py
+++ src/03_lobby.py
@@ -41,29 +41,62 @@ class BatteryBank:
                 if test_jolt > jolt:
                     jolt = test_jolt
         return jolt
-
+    
+    @functools.cache
+    def overdrive_joltage(self, n_batteries=12, debug=False):
+        battery_positions = list(range(len(self) - n_batteries, len(self)))
+        for battery_index in range(n_batteries):
+            test_position = battery_positions[battery_index]
+            battery_position = battery_positions[battery_index]
+            leftmost_position = battery_positions[battery_index - 1] + 1 if battery_index > 0 else 0
+            if debug:
+                print(f"Choosing battery {battery_index} starting at position {battery_position}")
+                print(f"  Testing positions from {test_position} down to {leftmost_position}")
+            while test_position >= leftmost_position:
+                test_jolt = self.batteries[test_position]
+                battery_jolt = self.batteries[battery_position]
+                if debug:
+                    print(f"  Testing position {test_position} with jolt {test_jolt} against current battery position {battery_position} with jolt {battery_jolt}")
+                if test_jolt >= battery_jolt:
+                    if debug:
+                        print(f"    Found better battery at position {test_position} with jolt {test_jolt}")
+                    battery_position = test_position
+                test_position -= 1
+            battery_positions[battery_index] = battery_position
+        str_jolt = "".join(str(self.batteries[pos]) for pos in battery_positions)
+        if debug:
+            print(f"Overdrive joltage for {self} with {n_batteries} batteries: {str_jolt}")
+        return int(str_jolt)
+            
 
 class BatteryBankCollection(list):
     def __init__(self, banks):
         super().__init__(banks)
         self.joltages = []
+        self.overdrive_joltages = []
     
     def __str__(self):
         return "\n".join(str(bank) for bank in self)
     
-    def calculate_joltages(self):
+    def calculate_joltages(self, debug=False):
         self.joltages = []
         for bank in self:
             self.joltages.append(bank.joltage)
     
+    def calculate_overdrive_joltages(self, n_batteries=12, debug=False):
+        self.overdrive_joltages = []
+        for bank in self:
+            self.overdrive_joltages.append(bank.overdrive_joltage(n_batteries, debug=debug))
+    
     def run_analysis(self, *, debug=False):
-        self.calculate_joltages()
+        self.calculate_joltages(debug=debug)
+        self.calculate_overdrive_joltages(debug=debug)
     
     def get_answer1(self, *, debug=False):
         return sum(self.joltages)
 
     def get_answer2(self, *, debug=False):
-        return None
+        return sum(self.overdrive_joltages)
     
     def get_answers(self, *, debug=False):
         answer1 = self.get_answer1(debug=debug)
