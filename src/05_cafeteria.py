70e821c65d76b9a8a9da6bf949b7cb781739cc68
Istvan Kleijn <istvan.kleijn@icr.ac.uk>
2025-12-17 15:43:46 +0000

    5-1: Cafeteria (fails)

---
+++ src/05_cafeteria.py
@@ -0,0 +1,98 @@
+import pathlib 
+
+
+DAY = 5
+
+test_input = """
+3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
+""".strip()
+
+
+def read_file(day: int):
+    file = pathlib.Path("data") / f"{day:02}.txt"
+    with open(file, "r") as handle:
+        lines = handle.readlines()
+    return(lines)
+
+def parse(lines, *, debug=False):
+    fresh_ranges = list()
+    lines_reversed = lines[::-1]
+    while ( (line := lines_reversed.pop()) != "" ):
+        start, stop = line.strip().split("-")
+        if debug:
+            print(f"Parsing {line=} into range: {start}-{stop}")
+        fresh_ranges.append(range(int(start), int(stop) + 1))
+    else:
+        if debug:
+            print("Finished parsing fresh ranges. Ingredients follow.")
+        ingredients = [int(line.strip()) for line in lines_reversed]
+        print(f"Parsed ingredients: {ingredients}")
+    inventory = Inventory(fresh_ranges, ingredients[::-1])
+    if debug:
+        print("Parsed inventory:\n", inventory)
+    return inventory
+
+class Inventory:
+    def __init__(self, fresh_ranges, ingredients):
+        self.fresh_ranges = fresh_ranges
+        self.ingredients = ingredients
+    
+    def __str__(self):
+        s = ""
+        for r in self.fresh_ranges:
+            s += f"{r.start}-{r.stop - 1}\n"
+        s += "\n"
+        for ingredient in self.ingredients:
+            s += f"{ingredient}\n"
+        return s
+
+    def determine_freshness(self, *, debug=False):
+        self.fresh_ingredients = list()
+        for ingredient in self.ingredients:
+            if any(ingredient in r for r in self.fresh_ranges):
+                if debug:
+                    print(f"Ingredient {ingredient} is fresh.")
+                self.fresh_ingredients.append(ingredient)
+            else:
+                if debug:
+                    print(f"Ingredient {ingredient} is spoiled.")
+
+    def run_analysis(self, *, debug=False):
+        self.determine_freshness(debug=debug)
+    
+    def get_answer1(self, *, debug=False):
+        return len(self.fresh_ingredients)
+
+    def get_answer2(self, *, debug=False):
+        return None
+    
+    def get_answers(self, *, debug=False):
+        answer1 = self.get_answer1(debug=debug)
+        answer2 = self.get_answer2(debug=debug)
+        return answer1, answer2
+
+
+def test():
+    lines = test_input.splitlines()
+    object = parse(lines, debug=True)
+    object.run_analysis(debug=True)
+    test1, test2 = object.get_answers(debug=True)
+    print(f"Test 1: {test1}\nTest 2: {test2}")
+
+if __name__ == "__main__":
+    test()
+    lines = read_file(DAY)
+    object = parse(lines, debug=True)
+    object.run_analysis()
+    answer1, answer2 = object.get_answers()
+    print(f"Answer 1: {answer1}\nAnswer 2: {answer2}")
